af3bf6d6164158d4ced3da78db7a1c0eda2f5eee
JC <jcteoh88@gmail.com>
2022-07-24 12:09:23 +0800

    Song list data recorded on bot server
    - SONGLIST.json for recording song data and song message ids
    - config.json to store tokens and messages and chat ids

--- main.py
+++ main.py
@@ -1,33 +1,47 @@
 # Import bot token to access bot via telegram http api
 from asyncore import dispatcher
-from tokenize import Token
-import TOKEN
+import json
 from telegram import Update
 from telegram.ext import Updater
 from telegram.ext import CallbackContext
 from telegram.ext import CommandHandler
 from telegram.ext import MessageHandler, Filters
 
+TOKEN = None
+SONG_LIST = []
+with open("config.json") as file:
+    TOKEN = json.load(file)
+    file.close()
 
 # Create a function called start
 def start(update: Update, context: CallbackContext):
-    context.bot.send_message(chat_id=update.effective_chat.id, text= TOKEN.welcomeText)
+    context.bot.send_message(chat_id=update.effective_chat.id, text= TOKEN.get("WelcomeText"))
     print("id: ", update.effective_chat.id)
 
 def echo(update: Update, context: CallbackContext):
-    for title in TOKEN.SONG_LIST:
-        if title[0] == update.message.text:
-            context.bot.forward_message(chat_id=update.effective_chat.id, from_chat_id=TOKEN.LIB_CHAT_ID, message_id=title[1])
+    for title in SONG_LIST:
+        for tag in title.get("tags"):
+            if tag.strip().upper() == update.message.text.strip().upper():
+                context.bot.forward_message(chat_id=update.effective_chat.id, from_chat_id=TOKEN.get("LIB_CHAT_ID"), message_id=title.get("id"))
     
 def audio(update: Update, context: CallbackContext):
-    if TOKEN.LIB_CHAT_ID == None:
-        TOKEN.LIB_CHAT_ID = update.effective_chat.id
-        print("Id for LIB channel: ", update.effective_chat.id)
-    if update.effective_chat.id != TOKEN.LIB_CHAT_ID: 
+    global SONG_LIST, TOKEN
+    if TOKEN.get("LIB_CHAT_ID") == 100:
+        TOKEN["LIB_CHAT_ID"] = update.effective_chat.id
+        with open("config.json", 'w') as file:
+            file.write(json.dumps(TOKEN))
+        print("Id for LIB channel updated to: ", update.effective_chat.id)
+    if update.effective_chat.id != TOKEN.get("LIB_CHAT_ID"): 
         return
     print(update.channel_post)
-    TOKEN.SONG_LIST.append([update.channel_post.caption, update.channel_post.message_id])
-
+    tags = update.channel_post.caption.split("::")
+    SONG_LIST.append({"tags": tags, "id": update.channel_post.message_id})
+    with open("SONGLIST.json", 'w') as file:
+        file.write(json.dumps(SONG_LIST))
+        file.close()
+    with open("SONGLIST.json", 'r') as file:
+        SONG_LIST = json.load(file)
+        file.close() 
 
 
 
@@ -42,7 +56,7 @@ def audio(update: Update, context: CallbackContext):
 
 if __name__ == '__main__':
     # Creates an updater object (used to fetch new updates from telegram bot)
-    updater = Updater(token=TOKEN.token, use_context=True)
+    updater = Updater(token=TOKEN.get("TOKEN"), use_context=True)
 
     # create variable for easy access to the dispatcher from the update object created
     dispatcher = updater.dispatcher
@@ -51,6 +65,11 @@ if __name__ == '__main__':
     import logging
     logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                         level=logging.INFO)
+    
+    # Open file for recording songs name, tags and ids
+    with open("SONGLIST.json", 'r') as file:
+        SONG_LIST = json.load(file)
+        file.close()
     
     # Create a handler to deal with /start command
     # Connect the handler to the start function created
